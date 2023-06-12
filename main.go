ebdf6403b3c83f24d7e94e53e2ee288ca57506ab
Vincencius Maxwell <vincenmaxwell@gmail.com>
2023-06-12 16:04:37 +0700

    Update Pagination

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -143,16 +144,19 @@ func CreateAccount(c *gin.Context) {
 		"error":   err,
 		"data":    &account})
 }
+
 func GetAllTransactions(c *gin.Context) {
 	var transactions []Transaction
-	if err := DB.Find(&transactions).Error; err != nil {
-		if err != nil {
-			c.JSON(500, gin.H{"message": "Error"})
-			return
-		}
-	}
+	var count int64
+	id := c.Param("id")
+	page, _ := strconv.Atoi(c.DefaultQuery("page", id))
+	pageSize, _ := strconv.Atoi(c.DefaultQuery("page_size", "10"))
+
+	DB.Model(&transactions).Count(&count)
+	DB.Offset((page - 1) * pageSize).Limit(pageSize).Find(&transactions)
 	c.JSON(200, gin.H{"message": &transactions})
 }
+
 func GetTransactionByStatus(c *gin.Context) {
 	var transactions []Transaction
 	status := c.Param("status")
@@ -238,14 +242,14 @@ func main() {
 	{
 		Admin.GET("/data-user", GetDataUser)
 		Admin.GET("/data-user/:id", GetDataUserById)
-		Admin.POST("/create-user", CreateAccount)
 		Admin.PUT("/data-user/:id", EditDataUser)
 		Admin.DELETE("/data-user/:id", DeleteDataUser)
 
-		Admin.GET("/get-transactions", GetAllTransactions)
-		Admin.GET("/get-transactions/:status", GetTransactionByStatus)
+		Admin.GET("/get-transactions/:id", GetAllTransactions)
+		Admin.GET("/get-transaction/:status", GetTransactionByStatus)
 		Admin.GET("/logout", Logout)
 	}
+	r.POST("/create-user", CreateAccount)
 	r.POST("/login", Login)
 
 	r.Run()
